b25d9b8b5c4df46d515dfdf07b14834414106622
Jill Centers <58017334+jlcenters@users.noreply.github.com>
2023-11-25 18:07:08 -0500

    first push

---
+++ Assets/Scripts/DataUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class DataUI : MonoBehaviour
+{
+    [SerializeField] private GameObject tankObject;
+    [SerializeField] private GameObject fuelObject;
+
+    [SerializeField] private Text tankTxt;
+    [SerializeField] private Text fuelTxt;
+    [SerializeField] private Text powerTxt;
+
+
+    private void Start()
+    {
+        tankTxt.text = tankObject.transform.position.ToString();
+    }
+}
---
+++ Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager : MonoBehaviour
+{
+    public GameObject objectPrefab;
+    public int spawnCount;
+    public Vector3 objectPosition;
+
+    private void Start()
+    {
+        for(int i = 0; i < spawnCount; i++)
+        {
+            GameObject obj = Instantiate(objectPrefab, new Vector3(Random.Range(-100, 100), //random x
+                                                        Random.Range(-100, 100), //random y
+                                                        objectPrefab.transform.position.z), //original prefab z
+                                                        Quaternion.identity); //keep original prefab rotation
+            Debug.Log("Fuel tank at " + obj.transform.position.ToString());
+        }
+    }
+}
